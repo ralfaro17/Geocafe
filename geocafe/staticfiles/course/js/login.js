4a925beeaa7e188a63c095b48e2f5990edaf579e
Roger Alfaro <123581434+ralfaro17@users.noreply.github.com>
2026-07-03 20:34:37 -0600

    Delete unnecessary geocafe/staticfiles directory

--- geocafe/staticfiles/course/js/login.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector('form');
-    form.addEventListener('submit', (event) => {
-        document.querySelector("button[type='submit']").disabled = true;
-    })
-});
